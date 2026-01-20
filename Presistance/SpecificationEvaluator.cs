d473a9ead686707c7a78f27e15fe2ab52aa8acf9
longway <longway.dev@proton.me>
2026-01-20 22:40:00 +0000

    feat: add core domain entities

---
+++ Presistance/SpecificationEvaluator.cs
@@ -0,0 +1,54 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance
+{
+    public static class SpecificationEvaluator
+    {
+        // Create methods to evaluate specifications here in the future (Create Quary)
+        // _dbcontext.products.Include(p=>p.BrandId).Include(p=>p.TypeId)
+
+        public static IQueryable<TEnity> CreateQuery<TEnity,TKey>(IQueryable<TEnity> EntireyPoint,ISpecification<TEnity,TKey> specification) where TEnity : BaseEnitiy<TKey>
+        {
+            var Query = EntireyPoint;
+            if (specification is not null)
+            {
+                if (specification.Criteria is not null)
+                {
+                    Query = Query.Where(specification.Criteria);
+                }
+                if (specification.IncludeExpression is not null && specification.IncludeExpression.Any())
+                {
+                    //foreach (var includeExpression in specification.IncludeExpression)
+                    //{
+                    //    Query = Query.Include(includeExpression);
+                    //}
+
+                    Query = specification.IncludeExpression
+                        .Aggregate(Query, (current, includeExpression) => current.Include(includeExpression));
+                }
+
+                if (specification.OrderBy is not null)
+                {
+                    Query = Query.OrderBy(specification.OrderBy);
+                }
+                if (specification.OrderByDescending is not null)
+                {
+                    Query = Query.OrderByDescending(specification.OrderByDescending);
+                }
+                if(specification.IsPaginated)
+                {
+                    Query = Query.Skip(specification.Skip).Take(specification.Take);
+                }
+            }
+            return Query;
+        }
+
+    }
+}
---
+++ ServiceAbstraction/IAuthenticationSerivce.cs
@@ -0,0 +1,26 @@
+﻿using ECommerce.Shared.CommonResult;
+using ECommerce.Shared.IdentityDtos;
+using ECommerce.Shared.OrderDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.ServiceAbstraction
+{
+    public interface IAuthenticationSerivce
+    {
+        // login =>Token ,DisplayName ,Email
+        // register=> UserDto
+        Task<Result<UserDto>> LoginAsync(LoginDto loginDto);
+         Task<Result<UserDto>> RegisterAsync(RegisterDto registerDto);
+
+        Task<bool> CheckEmailAsync(string email);
+        Task<Result<UserDto>> GetUserByEmailAsync(string email);
+
+        Task<Result<IdentityAddressDto>> GetCurrentUserAddressAsync(string email);
+        Task<Result<IdentityAddressDto>> UpdateCurrentUserAddressAsync(IdentityAddressDto addressDto, string email);
+
+    }
+}
