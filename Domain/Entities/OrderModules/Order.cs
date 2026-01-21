fb01813c01d46cdfb0cd8d9660144af6eb6d9b6c
longway <longway.dev@proton.me>
2026-01-21 11:21:00 +0000

    feat: implement repository abstractions

---
+++ Domain/Entities/OrderModules/Order.cs
@@ -0,0 +1,20 @@
+﻿
+
+namespace ECommerce.Domain.Entities.OrderModules
+{
+    public class Order : BaseEnitiy<Guid>
+    {
+        public string UserEmail { get; set; } = default!;
+        public DateTimeOffset OrderDate { get; set; } = DateTimeOffset.Now;
+        public OrderStatus OrderStatus { get; set; } = OrderStatus.pending;
+        public DeliveryMethod DeliveryMethod { get; set; } = default!; //Navigational property 1:m
+        public OrderAddress Address { get; set; } = default!;
+        public int DeliveryMethodId { get; set; } //fk
+        public ICollection<OrderItem> Items { get; set; } =[];
+        public decimal Subtotal { get; set; }
+        public decimal getTotal() => Subtotal + DeliveryMethod.Price;
+
+        public string? PaymentIntendId { get; set; }
+
+    }
+}
---
+++ Presistance/Data/Migrations/20260306201928_AddOrderAddressColumns.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ECommercePersistence.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddOrderAddressColumns : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Adress_Street",
+                table: "Order",
+                newName: "Address_Street");
+
+            migrationBuilder.RenameColumn(
+                name: "Adress_LastName",
+                table: "Order",
+                newName: "Address_LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Adress_FirstName",
+                table: "Order",
+                newName: "Address_FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "Adress_Country",
+                table: "Order",
+                newName: "Address_Country");
+
+            migrationBuilder.RenameColumn(
+                name: "Adress_City",
+                table: "Order",
+                newName: "Address_City");
+
+            migrationBuilder.AddColumn<string>(
+                name: "PaymentIntendId",
+                table: "Order",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentIntendId",
+                table: "Order");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_Street",
+                table: "Order",
+                newName: "Adress_Street");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_LastName",
+                table: "Order",
+                newName: "Adress_LastName");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_FirstName",
+                table: "Order",
+                newName: "Adress_FirstName");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_Country",
+                table: "Order",
+                newName: "Adress_Country");
+
+            migrationBuilder.RenameColumn(
+                name: "Address_City",
+                table: "Order",
+                newName: "Adress_City");
+        }
+    }
+}
