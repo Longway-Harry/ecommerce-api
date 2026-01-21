6e79eba97a722bbf128760e4a5a318813d1dc3c2
longway <longway.dev@proton.me>
2026-01-21 14:17:00 +0000

    feat: add specification query support

---
+++ Admin.Dashboard/Models/Products/ProductViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ECommerce.Domain.Entities.ProductModules;
+using System.ComponentModel.DataAnnotations;
+
+namespace Admin.Dashboard.Models.Products
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage ="Product Name is Required.")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Product Description is Required.")]
+        public string Description { get; set; }
+        public IFormFile Image { get; set; }
+
+        public string? PictureUrl { get; set; }
+        [Required(ErrorMessage = "Product Price is Required.")]
+        [Range(1,int.MaxValue)]
+        public decimal Price { get; set; }
+        [Required(ErrorMessage = "Product BrandId is Required.")]
+        public  int BrandId { get; set; }   
+        public ProductBrand? Brand { get; set; }
+        [Required(ErrorMessage = "Product TypeId is Required.")]
+        public int TypeId { get; set; }
+        public ProductType? Type { get; set; }
+
+    }
+}
---
+++ Shared/OrderDtos/OrderDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.OrderDtos
+{
+   public record OrderDto(string BasketId,int DeliveryMethodId,AddressDto ShipToAddress);
+}
