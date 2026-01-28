162cd95f8ffdcc524e5ff6febfe56d9a13318188
longway <longway.dev@proton.me>
2026-01-28 22:57:00 +0000

    refactor: isolate infrastructure services

---
+++ Service/MappingProfiles/AuthProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using ECommerce.Domain.Entities.IdentityModule;
+using ECommerce.Shared.IdentityDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service.MappingProfiles
+{
+    public  class AuthProfile:Profile
+    {
+        public AuthProfile()
+        {
+            CreateMap<Address,IdentityAddressDto>().ReverseMap();
+        }
+    }
+}
---
+++ Web/GlobalUsings.cs
@@ -0,0 +1,28 @@
+﻿// --- Microsoft Frameworks & Identity ---
+global using Microsoft.AspNetCore.Mvc;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.AspNetCore.Identity;
+global using Microsoft.AspNetCore.Authentication.JwtBearer;
+global using Microsoft.IdentityModel.Tokens;
+
+// --- Third-Party Libraries ---
+global using StackExchange.Redis;
+global using System.Text;
+
+// ---  Onion Layers (Infrastructure/Persistence) ---
+global using ECommerce.Presistance.Data.DBContexts;
+global using ECommerce.Presistance.Data.DataSeeding;
+global using ECommerce.Presistance.IdentityData.DbContext;
+global using ECommerce.Presistance.IdentityData.DataSeed;
+global using ECommerce.Presistance.Repository;
+
+// ---  Onion Layers (Application/Domain) ---
+global using ECommerce.Domain.Contracts;
+global using ECommerce.Domain.Entities.IdentityModule;
+global using ECommerce.Service;
+global using ECommerce.ServiceAbstraction;
+
+// --- Web Specific (Extensions & Middlewares) ---
+global using ECommerceWeb.CustomeMiddleWare;
+global using ECommerceWeb.Extentions;
+global using ECommerceWeb.Factory;
