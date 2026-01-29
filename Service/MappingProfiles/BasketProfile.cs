9104a2679f5157862533b6cf14932ccc88ab8e59
longway <longway.dev@proton.me>
2026-01-29 13:04:00 +0000

    feat: implement product filtering

---
+++ Service/MappingProfiles/BasketProfile.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using ECommerce.Domain.Entities.BasketModules;
+using ECommerce.Shared.BasketDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service.MappingProfiles
+{
+    public class BasketProfile:Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap <CustomerBasket, BasketDtos>().ReverseMap();
+            CreateMap<BasketItem, BasketItemDtos>().ReverseMap();
+
+        }
+    }
+}
