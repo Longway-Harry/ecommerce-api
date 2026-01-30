a4f002cf8f8deb21518c782531bd8bafa742b9e7
longway <longway.dev@proton.me>
2026-01-30 20:53:00 +0000

    feat: add identity management

---
+++ Presistance/Repository/BasketRepository.cs
@@ -0,0 +1,54 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities.BasketModules;
+using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Repository
+{
+    public class BasketRepository : IBasketRepository
+    {
+      
+        private readonly IDatabase _database;
+
+
+        public BasketRepository(IConnectionMultiplexer connection)
+        {
+            _database = connection.GetDatabase();
+        }
+        public async Task<CustomerBasket?> CreateOrUpdateBasketAsync(CustomerBasket basket, TimeSpan timetolive = default)
+        {
+            var JsonBasket =JsonSerializer.Serialize(basket);
+            var IsCreatedOrUpdated = await  _database.StringSetAsync(basket.Id, JsonBasket,
+                (timetolive==default)?TimeSpan.FromDays(7):timetolive);
+            if (IsCreatedOrUpdated)
+            {
+                return await GetBasketAsync(basket.Id);
+            }
+            else
+            { 
+                return null;
+            }
+        }
+
+        public async Task<bool> DeleteCustomerBasketAsync(string basketId)=> await _database.KeyDeleteAsync(basketId);
+
+
+        public async Task<CustomerBasket?> GetBasketAsync(string basketId)
+        {
+           var Basket= await _database.StringGetAsync(basketId);
+            if (Basket.IsNullOrEmpty)
+            {
+                return null;
+            }
+            else
+            {
+                return JsonSerializer.Deserialize<CustomerBasket>(Basket!);
+            }
+        }
+    }
+}
---
+++ Presistance/Repository/GenericRepository.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities;
+using ECommerce.Presistance.Data.DBContexts;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.Repository
+{
+    public class GenericRepository<TEnity, TKey> : IGenericRepository<TEnity, TKey> where TEnity : BaseEnitiy<TKey>
+    {
+        private readonly StoreDbContext _dbContext;
+
+        public GenericRepository(StoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task AddAsync(TEnity entity)=> await _dbContext.Set<TEnity>().AddAsync(entity);
+
+        public async Task<int> CountAsync(ISpecification<TEnity, TKey> specification)
+        {
+           return await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEnity>(), specification).CountAsync(); // we are using CountAsync to get count of entities based on specification
+        }
+
+        public async Task<IEnumerable<TEnity>> GetAllAsync() =>await _dbContext.Set<TEnity>().ToListAsync();
+
+        public async Task<IEnumerable<TEnity>> GetAllAsync(ISpecification<TEnity, TKey> specification)
+        {
+           var Query = SpecificationEvaluator.CreateQuery(_dbContext.Set<TEnity>(), specification); 
+
+              return await Query.ToListAsync(); // we are using ToListAsync to get all entities based on specification
+        }
+
+        public async Task<TEnity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEnity>().FindAsync(id);
+
+        public async Task<TEnity?> GetByIdAsync(ISpecification<TEnity, TKey> specification)
+        {
+           var Query = await SpecificationEvaluator.CreateQuery(_dbContext.Set<TEnity>(), specification)
+                .FirstOrDefaultAsync(); // we are using FirstOrDefaultAsync to get single entity based on specification
+            return Query;
+        }
+
+        public void Remove(TEnity entity) => _dbContext.Set<TEnity>().Remove(entity);
+
+
+        public void Update(TEnity entity) => _dbContext.Set<TEnity>().Update(entity);
+
+    }
+}
