b6d590b5c9b3ebfbea42ae5f0a8a803e04f9b063
longway <longway.dev@proton.me>
2026-01-31 16:03:00 +0000

    fix: improve middleware exception handling

---
+++ Domain/Contracts/ICachRepository.cs
@@ -0,0 +1,11 @@
+﻿
+
+namespace ECommerce.Domain.Contracts
+{
+    public interface ICachRepository
+    {
+        Task<string?> GetAsync(string Cachkey);
+        Task SetAsync(string Cachkey, string CachValue, TimeSpan timetolive);
+
+    }
+}
