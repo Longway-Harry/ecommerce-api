18c2705912a54c154cbdf92a7997371d554d8bcc
longway <longway.dev@proton.me>
2026-01-31 18:41:00 +0000

    feat: add mapping profiles

---
+++ Service/Specifications/OrderSpecifications.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Domain.Entities.OrderModules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service.Specifications
+{
+    public class OrderSpecifications:BaseSpecification<Order,Guid>
+    {
+        public OrderSpecifications(Guid id ,string userEmail):base(o=>o.Id==id && o.UserEmail.ToLower()==userEmail.ToLower())
+        {
+            IncludeExpression.Add(o => o.Items);
+            IncludeExpression.Add(o => o.DeliveryMethod);
+        }
+        public OrderSpecifications(string userEmail):base(o=>o.UserEmail.ToLower() == userEmail.ToLower())
+        {
+            IncludeExpression.Add(o => o.Items);
+            IncludeExpression.Add(o => o.DeliveryMethod);
+            AddOrderByDescending(o => o.OrderDate);
+        }
+    }
+}
---
+++ Shared/OrderDtos/OrderItemDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Shared.OrderDtos
+{
+    public class OrderItemDto
+    {
+        public string ProductName { get; init; } = string.Empty;
+        public string PictureUrl { get; init; } = string.Empty;
+        public decimal Price { get; init; }
+        public int Quantity { get; init; }
+
+    }
+}
