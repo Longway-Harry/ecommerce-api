2bc89a352e70d4029e5667e8cafb61286f88dbf9
longway <longway.dev@proton.me>
2026-02-06 18:14:00 +0000

    feat: add redis basket persistence

---
+++ Domain/Entities/OrderModules/ProductItemOrder.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ECommerce.Domain.Entities.OrderModules
+{
+    // part of the orderitem Tabel, we need to save the product details at the time of order
+    public class ProductItemOrder
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = default!;
+        public string PictureUrl { get; set; } = default!;
+    }
+}
