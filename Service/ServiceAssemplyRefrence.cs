7fca03509bf2146e7f0271a701cda0b4d55a86e9
longway <longway.dev@proton.me>
2026-02-09 22:51:00 +0000

    refactor: simplify service registration

---
+++ Service/ServiceAssemplyRefrence.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Service
+{
+    public class ServiceAssemplyRefrence
+    {
+        // Marker class used to obtain the Service assembly reference.
+        // This helps frameworks such as AutoMapper scan the assembly
+        // and register profiles automatically.
+    }
+}
