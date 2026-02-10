e6a91bdedd2449aa325f838dcde1649f3803bd36
longway <longway.dev@proton.me>
2026-02-10 18:42:00 +0000

    fix: resolve pagination edge cases

---
+++ Admin.Dashboard/Controllers/ProductBrandsController.cs
@@ -0,0 +1,41 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities.ProductModules;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Admin.Dashboard.Controllers
+{
+    public class ProductBrandsController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductBrandsController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var brands = await _unitOfWork.GetRepository<ProductBrand, int>().GetAllAsync();
+            return View(brands);
+        }
+        public async Task<IActionResult> Create(ProductBrand brand)
+        {
+            if (ModelState.IsValid)
+            {
+                await _unitOfWork.GetRepository<ProductBrand, int>().AddAsync(brand);
+                await _unitOfWork.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(brand);
+        }
+
+        public async Task<IActionResult> Delete(int id)
+        {
+            var brand = await _unitOfWork.GetRepository<ProductBrand, int>().GetByIdAsync(id);
+            if (brand is null) return NotFound();
+            _unitOfWork.GetRepository<ProductBrand, int>().Remove(brand);
+            await _unitOfWork.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
---
+++ ServiceAbstraction/IBasketServices.cs
@@ -0,0 +1,17 @@
+﻿using ECommerce.Shared.BasketDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.ServiceAbstraction
+{
+    public interface IBasketServices
+    {
+        Task<BasketDtos> GetBasketAsync(string Id);
+        Task<BasketDtos> CreateOrUpdateBasketAsync(BasketDtos basket);
+        Task<bool> DeleteBasketAsync(string Id);
+
+    }
+}
