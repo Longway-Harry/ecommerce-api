afcf0a79927402dfc609d26bc789301adec012a8
longway <longway.dev@proton.me>
2026-02-11 14:20:00 +0000

    refactor: isolate infrastructure services

---
+++ Domain/Entities/OrderModules/DeliveryMethod.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace ECommerce.Domain.Entities.OrderModules
+{
+    public class DeliveryMethod: BaseEnitiy<int>
+    {
+        public string ShortName { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string DeliveryTime { get; set; } = default!;
+        public decimal Price { get; set; }
+
+    }
+}
---
+++ Shared/IdentityDtos/UserDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Shared.IdentityDtos
+{
+    public  record UserDto(string Email,string DisplayName ,string Token);
+   
+}
