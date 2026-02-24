e21acdec1f255eead6b39a1dcb92c897f83a8e2d
longway <longway.dev@proton.me>
2026-02-24 10:22:00 +0000

    fix: resolve pagination edge cases

---
+++ Presentation/Controllers/ordersController.cs
@@ -0,0 +1,65 @@
+﻿using ECommerce.ServiceAbstraction;
+using ECommerce.Shared.OrderDtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presentation.Controllers
+{
+    public class ordersController: ApiBaseController
+    {
+        private readonly IOrderService _orderService;
+
+        public ordersController(IOrderService orderService)
+        {
+           _orderService = orderService;
+        }
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<OrderToReturnDto>> CreateOrder(OrderDto orderDto)
+        {
+            var Email = User.FindFirstValue(ClaimTypes.Email);
+            var result= await _orderService.CreateOrderAsync( orderDto, Email!);
+            return HandelResult(result);
+
+        }
+
+        [HttpGet("deliveryMethods")]
+        public async Task<IActionResult> GetAllDeliveryMethods()
+        {
+            var result = await _orderService.GetAllDeliveryMethodsAsync();
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public async Task<IActionResult> GetOrderForSpecificUser()
+        {
+            var Email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await _orderService.GetOrdersForUserAsync(Email!);
+            return Ok(result);
+
+        }
+
+        [HttpGet("{id}")]
+        [Authorize]
+        public async Task<ActionResult<OrderToReturnDto>> GetOrderByIdForSpecificUser(Guid id)
+        {
+            var Email = User.FindFirstValue(ClaimTypes.Email);
+            var result = await _orderService.GetOrderByIdAsync(id, Email!);
+            return HandelResult(result);
+
+        }
+
+
+
+
+
+
+        }
+}
