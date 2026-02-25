22cf6113de4a7d9e38aef1d7ab56d7b32b209960
longway <longway.dev@proton.me>
2026-02-25 10:55:00 +0000

    refactor: isolate infrastructure services

---
+++ Domain/Entities/BasketModules/CustomerBasket.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace ECommerce.Domain.Entities.BasketModules
+{
+    public class CustomerBasket
+    {
+        public string Id { get; set; } =default!;
+        public ICollection<BasketItem> Items { get; set; } = new List<BasketItem>();
+        // payment
+        public int? DeliveryMethodId { get; set; }
+        public string? PaymentIntentId { get; set; }
+        public string? ClientSecret { get; set; }
+        public decimal ? ShippingCost { get; set; }
+
+
+    }
+}
