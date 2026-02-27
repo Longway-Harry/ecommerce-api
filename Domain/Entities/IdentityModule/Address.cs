db23ad6b110fbd4ccf51dbc2e3a03c2df0dadfd4
longway <longway.dev@proton.me>
2026-02-27 16:25:00 +0000

    feat: add identity management

---
+++ Domain/Entities/IdentityModule/Address.cs
@@ -0,0 +1,17 @@
+﻿namespace ECommerce.Domain.Entities.IdentityModule
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string City { get; set; } = default!;
+        public string Street { get; set; } = default!;
+
+        public string Country { get; set; } = default!;
+
+        public string FistName { get; set; } = default!;
+        public string LastName { get; set; } = default!;
+
+        public ApplicationUser user { get; set; } = default!;
+        public string UserId { get; set; } = default!; // fk [Unique Constraint ]
+    }
+}
---
+++ Presistance/IdentityData/DataSeed/IdentityDataInitalizer.cs
@@ -0,0 +1,66 @@
+﻿using ECommerce.Domain.Contracts;
+using ECommerce.Domain.Entities.IdentityModule;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Presistance.IdentityData.DataSeed
+{
+    public class IdentityDataInitalizer : IDataInitializer
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public IdentityDataInitalizer( UserManager<ApplicationUser> userManager,RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+           _roleManager = roleManager;
+        }
+        public async Task InitializeAsync()
+        {
+            try
+            {
+                if (!_roleManager.Roles.Any())
+                {
+                    await _roleManager.CreateAsync(new IdentityRole("Admin"));
+                    await _roleManager.CreateAsync(new IdentityRole("SuperAdmin"));
+                }
+
+                if (!_userManager.Users.Any())
+                {
+                    var user01 = new ApplicationUser
+                    {
+                        DisplayName = "Abdalla Aboaziz",
+                        UserName = "AbdallaAboaziz",
+                        Email = "abdallaaboaziz@gmail.com",
+                        PhoneNumber = "01277353904",
+
+                    };
+                    var user02 = new ApplicationUser
+                    {
+                        DisplayName = "Ahmed ali",
+                        UserName = "AhmedAli",
+                        Email = "AhmedAli@gmail.com",
+                        PhoneNumber = "01277353111",
+                    };
+                    // Create Users
+                    await _userManager.CreateAsync(user01, "Admin@123");
+                    await _userManager.CreateAsync(user02, "Admin@123");
+                    // Assign Roles
+                    await _userManager.AddToRoleAsync(user01, "SuperAdmin");
+                    await _userManager.AddToRoleAsync(user02, "Admin");
+
+                }
+
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine($"Error While Seeding Data {ex}");
+            }
+        }
+    }
+}
